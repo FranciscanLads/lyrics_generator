56ca4ba399a39cc67b7885d6e7c4dff2da74808b
Shubhendu Singh <itstimetogetout@Shubhendus-MacBook-Pro.local>
2020-04-06 19:12:17 +0530

    added initial scraping files

---
+++ coldplay_test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = None
+
+try:
+    driver = webdriver.Chrome()
+    action=ActionChains(driver)
+    driver.get("https://www.azlyrics.com/c/coldplay.html")
+    driver.implicitly_wait(2)
+    links = driver.find_elements_by_xpath("//*[@id='listAlbum']")
+    driver.implicitly_wait(2)
+    try:
+        element = WebDriverWait(driver, 6).until(EC.presence_of_element_located((By.ID, "listAlbum")))
+    finally:
+        with open("coldplay"+".html", "w", encoding="utf8") as f:
+            f.write(driver.page_source)
+
+finally:
+    if driver is not None:
+        driver.close()
